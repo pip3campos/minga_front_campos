580e24973f7f7a4795c800f39d2b72614019714b
pip3campos <felipeacampose@gmail.com>
2023-08-30 12:37:53 -0400

    display

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,7 +3,7 @@ import Arrow from "./Arrow";
 export default function Carousel () {
     return (
         <div className='max-md:hidden w-full bg-white text-white flex justify-center items-center my-12 divCarrousel'>
-            <Arrow direccion="rotate-180 left-20" />
+            <Arrow direccion="rotate-180 left-20 cursor-pointer" />
             <div className='flex w-11/12 rounded-md justify-around carrusel'>
                 <img className='w-64 h-80 img4' src="./images/image4.png" alt="" />
                 <img className='w-40 h-72 img3' src="./images/image3.png" alt="" />
@@ -12,7 +12,7 @@ export default function Carousel () {
                     <p className='text-sm'>Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out</p>
                 </div>
             </div>
-            <Arrow direccion="right-20" />
+            <Arrow direccion="right-20 cursor-pointer" />
         </div>
     )
 }
