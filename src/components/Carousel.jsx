a44150c43accdc330c56bb99ec58db87e78ace78
pip3campos <felipeacampose@gmail.com>
2023-08-30 16:08:57 -0400

    carousel listo

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,33 +1,35 @@
 import { useEffect, useState } from "react";
 import Arrow from "./Arrow";
-import axios from 'axios'
+import axios from "axios";
 
 export default function Carousel () {
     let [categories, setCategories] = useState([])
     let [counter, setCounter] = useState(0)
-    function fetchData() {
-        axios('https://minga-back-vyqy.onrender.com/categories')
-        .then( res => setCategories(res.data.categories))
-        .catch( e => console.log(e))
+    let url = "https://minga-back-vyqy.onrender.com/categories"
+    function getData(){
+        axios(url)
+        .then( res => {
+            return setCategories(res.data.categories)
+        })
+        .catch( err => console.log(err))
     }
     useEffect(() => {
-        fetchData(),
-        []
-    })
+        getData()
+    }, [])
     let next = () => ( counter !== categories.length - 1 ) ? setCounter(counter + 1) : setCounter(0)
     let prev = () => ( counter !== 0 ) ? setCounter(counter - 1) : setCounter(categories.length - 1)
     return (
         <div className='max-md:hidden w-full bg-white text-white flex justify-center items-center my-12 divCarrousel'>
-            <Arrow prev={prev} direccion="rotate-180 left-20 cursor-pointer" />
+            <Arrow onClick={prev} direccion="rotate-180 left-20 cursor-pointer" />
             <div className='flex w-11/12 rounded-md justify-around carrusel'>
-                <img className='w-64 h-80 img4' src="{categories[counter]?.character_photo}" alt="" />
-                <img className='w-40 h-72 img3' src="{categories[counter]?.cover_photo}" alt="" />
+                <img className='w-64 h-80 img4' src={categories[counter]?.character_photo} alt="" />
+                <img className='w-40 h-72 img3' src={categories[counter]?.cover_photo} alt="" />
                 <div className='self-center md:w-48 lg:w-80'>
                     <h3 className='text-2xl'>{categories[counter]?.name}</h3>
                     <p className='text-sm'>{categories[counter]?.description}</p>
                 </div>
             </div>
-            <Arrow next={next} direccion="right-20 cursor-pointer" />
+            <Arrow onClick={next} direccion="right-20 cursor-pointer" />
         </div>
     )
 }
