63538caec9a610a7358d7373f02f007c9a928cbd
pip3campos <felipeacampose@gmail.com>
2023-08-30 10:51:31 -0400

    layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
+import Main from './layouts/Main'
 import Index from './components/Index'
 
 export default function App() {
   return (
-    <>
-      <NavBar />
-      <Index />
-      <Footer />
-    </>
+    <Main>
+    <Index />
+    </Main>
   )
 }
